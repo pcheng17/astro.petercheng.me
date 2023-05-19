d8ec1aba3f4a38f00de2bbb8caff5754b02d585f
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-05-18 22:23:27 -0700

    fix: proper ordering in rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,17 +6,20 @@ export async function get() {
   const allPosts = await getCollection("posts", ({ data }) => {
     return data.draft !== true;
   });
+  const sortedPosts = allPosts.sort((a, b) => {
+    return a.data.pubDate < b.data.pubDate ? 1 : -1;
+  });
   return rss({
     title: SITE.title,
     description: SITE.description,
     site: "https://inquisitive-gelato-e04f0f.netlify.app/",
-    items: allPosts.map((post) => ({
+    items: sortedPosts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       link: `/posts/${post.slug}`,
     })),
-    stylesheet: "/rss/styles.xsl",
+    // stylesheet: "/rss/styles.xsl",
     customData: `<language>en-us</language>`,
   });
 }
