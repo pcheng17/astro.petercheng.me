7fb86ef471df14cb93e8571e5d91d1f2ff6d83fa
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-08-06 00:58:58 -0700

    Fix titles

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,7 @@ import type { Site } from "./types";
 export const SITE: Site = {
   site: "https://petercheng.me",
   author: "Peter Cheng",
-  bio: "Mathematician. Software engineer.",
-  description:
-    "Peter Cheng's website. A little bit of everything involving my work, life, and hobbies.",
-  title: "Peter Cheng's website",
+  bio: "Mathematician. Software engineer. Creator.",
+  description: "A little bit of everything involving my work, life, and hobbies.",
+  title: "Peter Cheng",
 };
