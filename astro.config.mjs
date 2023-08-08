adb0e798db613d58d25a3363523c0c966e365b97
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-08-08 11:18:11 -0700

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
+import vercel from "@astrojs/vercel/static";
 // import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel({ analytics: true }),
   integrations: [
     partytown({
       config: {
